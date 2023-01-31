e608af15b1c9aee79e5b69c0b188e3f5945840df
Ansel <mgolriz6@gmail.com>
2023-01-31 03:09:16 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+def mad_libs_generator():
+    adjective_1 = input("Enter an adjective: ")
+    adjective_2 = input("Enter another adjective: ")
+    noun_1 = input("Enter a noun: ")
+    noun_2 = input("Enter another noun: ")
+    verb = input("Enter a verb: ")
+    
+    print("The " + adjective_1 + " and " + adjective_2 + " " + noun_1 + " " + verb + " the " + noun_2 + ".")
+
+mad_libs_generator()
